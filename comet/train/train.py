8458481c2c5271dc564399534cb5295c2c8ccd3d
puraminy <pouramini@gmail.com>
2021-11-24 20:31:05 +0330

    commit

--- comet/train/train.py
+++ comet/train/train.py
@@ -734,10 +734,9 @@ def train(model_id, experiment, qtemp, anstemp, extemp, method, train_samples, v
                      truncation=True,
                      max_length=256, 
                      padding='max_length')
-         labels = tokenized['input_ids']
+         labels = new_batch['input_ids']
          labels[labels==tokenizer.pad_token_id] = -100
          new_batch['labels']=labels
-         new_batch['attention_mask']=tokenized["attention_mask"]
          return new_batch #,references
     #%% build dataloader
     if  "t5" in model_id:
