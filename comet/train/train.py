546753a0b710e3ebde628ab5d7a9a412ad367624
puraminy <pouramini@gmail.com>
2021-11-24 16:59:31 +0330

    commit

--- comet/train/train.py
+++ comet/train/train.py
@@ -724,11 +724,11 @@ def train(model_id, experiment, qtemp, anstemp, extemp, method, train_samples, v
          outputs =list(responses)
          new_batch = tokenizer(outputs,return_tensors='pt',
                  truncation=True,
-                 max_length=512,
+                 max_length=256,
                  padding='max_length')
          tokenized = tokenizer(outputs,return_tensors='pt',
                      truncation=True,
-                     max_length=512, 
+                     max_length=256, 
                      padding='max_length')
          labels = tokenized['input_ids']
          #labels[labels==tokenizer.pad_token_id] = -100
