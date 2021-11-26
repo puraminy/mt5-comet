0bf7ea84be6d1502ed358de4a956036a01c2127d
puraminy <pouramini@gmail.com>
2021-11-26 10:54:25 +0330

    commit

--- comet/train/common.py
+++ comet/train/common.py
@@ -307,7 +307,10 @@ def filter_inputs(include, exclude, lang):
 #tttttttttt
 def create_templates(method, gen_pos="end", prompt_pos="start"):
        extemp = ""
-       if method == "sup-pred-enfa":
+       if method == "rel":
+           qtemp = "{event} {ph} {resp}"
+           anstemp = "{ph}"
+       elif method == "sup-pred-enfa":
            qtemp = "{input_text} {enc_token} {gen_fa}"
            anstemp = "{input_text_fa} {dec_token} {target_text_fa}"
        elif method == "sup-enfa":
