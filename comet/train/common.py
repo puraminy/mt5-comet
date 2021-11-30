0f68dd5b4b32ac1bf00caea25cc707ae79f1f6f3
puraminy <pouramini@gmail.com>
2021-11-30 22:04:38 +0330

    commit

--- comet/train/common.py
+++ comet/train/common.py
@@ -542,7 +542,7 @@ def create_templates(method, gen_pos="end", prompt_pos="end"):
            extemp = "{input_text} {rel_natural_en} {target_text} {end} \n"
            anstemp = "{ph} {event} {rel_natural} {resp} {end}"
        elif method == "gpt-event-resp-n":
-           qtemp = "{examples}"
+           qtemp = "{examples} {enc_token}"
            extemp = "{enc_token} {input_text} {rel_natural_en} {target_text} {end} \n"
            anstemp = "{event} {rel_natural} {resp} {end}"
        elif method == "fa-event-n":
