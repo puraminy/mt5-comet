79691592edcbd3267fd29e0d0b2a291d30e1e16f
puraminy <pouramini@gmail.com>
2021-12-11 16:36:43 +0330

    commit

--- comet/train/score.py
+++ comet/train/score.py
@@ -17,17 +17,24 @@ def bart_score(model, df, before, after, col1, col2, score_col, cpu):
 
   scores = []
   pbar = tqdm(total = len(df))
+  rows = []
   for i, row in df.iterrows(): 
       rel = row.prefix
+      data = {}
       langs = row.langs
       from_, to_ = langs.split("2")
       s1 = row.input_text
       s1 = s1.replace("##","").replace("  "," ").strip()
       s2 = row[col1]
       s2 = relation_natural_mappings[rel][to_] + " " + s2
+      data["s1"] = s1
+      data["s2"] = s2
+      data["langs"] = langs
       pbar.update()
       score = model.score([s1], [s2], batch_size=4) # generation scores from the first list of texts to the second list of texts.
       scores.append("{:.5f}".format(score[0]))
+      data["score"] = score
+      rows.append(data)
       if i < 5:
           print(i)
           print(s1)
@@ -35,7 +42,8 @@ def bart_score(model, df, before, after, col1, col2, score_col, cpu):
           print(score)
   
   df[score_col] = scores
-  return df
+  new_df = pd.DataFrame(data=rows)
+  return df, new_df
 
 def bert_score(model, df, before, after, col1, col2, score_col, cpu):
   if after > 0:
@@ -43,12 +51,48 @@ def bert_score(model, df, before, after, col1, col2, score_col, cpu):
   else:
     df = df.truncate(before=before)
   
-  col1_val = df[col1].astype(str).tolist()
-  col2_val = df[col2].astype(str).tolist()
-  
   device = torch.device("cuda")
   if cpu:
       device = torch.device("cpu")
+
+  rows = []
+  for idx, row in tqdm(df.iterrows(), total=len(df)):
+        data = {}
+        data["s1"] = row[col1]
+        data["s2"] = row[col2]
+        sents1 = row[col1].split("<br />")
+        sents2 = row[col2].split("<br />")
+
+        #Compute embeddings
+        embeddings1 = model.encode(sents1, device=device, convert_to_tensor=True)
+        embeddings2 = model.encode(sents2, device=device, convert_to_tensor=True)
+
+        #Compute cosine-similarities for each sentence with each other sentence
+        cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
+        #print(cosine_scores)
+
+        #Find the pairs with the highest cosine similarity scores
+        pairs = []
+        rows = cosine_scores.shape[0]
+        cols = cosine_scores.shape[1]
+        for i in range(rows):
+            for j in range(cols):
+                pairs.append({'index': [i, j], 'score': cosine_scores[i][j]})
+                #print({'index': [i, j], 'score': cosine_scores[i][j]})
+
+        #Sort scores in decreasing order
+        pairs = sorted(pairs, key=lambda x: x['score'], reverse=True)
+
+        top = pairs[0]
+
+        data["target"] = df.loc[idx, "best_target"] = sents2[top["index"][0]]
+        data["prediction"] = df.loc[idx, "best_pred"] = sents2[top["index"][1]]
+        data[score_col] = df.loc[idx, score_col] = "{:.4f}".format(top["score"])
+
+    new_df = pd.DataFrame(data=data)
+
+  return df,new_df
+
   col1_val_emb = model.encode(col1_val, device=device, convert_to_tensor=True)
   col2_val_emb = model.encode(col2_val,  device=device, convert_to_tensor=True)
   
@@ -57,11 +101,16 @@ def bert_score(model, df, before, after, col1, col2, score_col, cpu):
   
   #Output the pairs with their score
   scores = []
-  for i in tqdm(range(len(df)), total = len(df)):
+  rows=[]
+  pbar = tqdm(total=len(df)
+  for i, row in df.iterrows():
+      data[col1] = 
+      pbar.update()
       scores.append("{:.4f}".format(cosine_scores[i][i]))
   
   df[score_col] = scores
-  return df
+  new_df = pd.DataFrame(data=rows)
+  return df, new_df
 
 
 from pathlib import Path
@@ -157,20 +206,23 @@ def main(fname, model_name, path, step, col1, col2, score_col, cpu, concat):
     after = step
     if after < 0:
         if "bert_score" in score_col:
-            df = bert_score(model, srcdf, before, -1, col1, col2, score_col, cpu)
+            df,new_df = bert_score(model, srcdf, before, -1, col1, col2, score_col, cpu)
         else:
-            df = bart_score(model, srcdf, before, -1, col1, col2, score_col, cpu)
+            df,new_df = bart_score(model, srcdf, before, -1, col1, col2, score_col, cpu)
     else:
         df_old = None
+        new_df_old = None
         while True:
           print(before, "-", after)
           if "bert_score" in score_col:
-             df = bert_score(model,srcdf, before, after, col1, col2, score_col, cpu)
+             df,new_df = bert_score(model,srcdf, before, after, col1, col2, score_col, cpu)
           else:
-             df = bart_score(model,srcdf, before, after, col1, col2, score_col, cpu)
+             df,new_df = bart_score(model,srcdf, before, after, col1, col2, score_col, cpu)
           if df_old is not None:
               df = pd.concat([df_old, df], ignore_index=True)
+              new_df = pd.concat([new_df_old, new_df], ignore_index=True)
           df_old = df
+          new_df_old = new_df
           before = after + 1
           after += step
           if after >= len(srcdf):
@@ -187,8 +239,12 @@ def main(fname, model_name, path, step, col1, col2, score_col, cpu, concat):
 
     out1 = resPath + "/" + Path(fname).stem  + ".tsv" 
     out2 = logPath + "/" + Path(fname).stem  + ".tsv" 
+    out_new1 = resPath + "/new_" + Path(fname).stem  + ".tsv" 
+    out_new2 = logPath + "/new_" + Path(fname).stem  + ".tsv" 
     df.to_csv(out1, sep="\t", index=False)
     df.to_csv(out2, sep="\t", index=False)
+    new_df.to_csv(out_new1, sep="\t", index=False)
+    new_df.to_csv(out_new2, sep="\t", index=False)
     print(len(df))
     out1 = out1.replace("/content/drive/MyDrive/", "")
     print(f"rcopy nlp:{out1} . ")
