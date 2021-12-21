8cf0cff097377f069226481d335f5bb657a86a38
Ahmad <pouramini@gmail.com>
2021-12-21 12:24:48 +0330

    commit

--- comet/train/run.sh
+++ comet/train/run.sh
@@ -1,9 +1,9 @@
 
 
-python train.py train t5-large -n 0 -even -exp t5-large-unsup-wrap-tokens-lstm-embed \
-  -eps 1 -lang en-en -vp atomic/val_all_rels.tsv  \
+python train.py train t5-large -n 0 \
+  -eps 1 -lang en-en -vp atomic/test.tsv -tp atomic/train.tsv  \
   -load /home/pouramini/pret -gp greedy@True \
-  -save /home/pouramini/pret -ow unsup-wrap-tokens -bs 4 \
-  -mt unsup-wrap-tokens -w -et lstm -ot adam -f -bs 60 #-fw rel_tokens \
+  -save /home/pouramini/pret -ow unsup-wrap-tokens-full \
+  -mt unsup-wrap-tokens -w -et lstm -ot adam -f -bs 30 #-fw rel_tokens \
 		   
 
