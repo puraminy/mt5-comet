314fb274d225e1cd1017d89254e982123b83fd3c
puraminy <pouramini@gmail.com>
2022-11-07 20:31:56 +0330

    commit

--- comet/train.sh
+++ comet/train.sh
@@ -39,13 +39,13 @@ alias runlite="python ${home}/mt5-comet/comet/train/train.py"
 folder=${PWD##*/}          
 cp train.sh ..
 
-test=5
-train=10
+test=100
+train=200
+exp=merge
 
-runlite run -lp $home/logs -bc base -ov $g2 -var method=unsup-wrap-nat--rel_filter=xIntent#xAttr#multi--train_samples=$train--epochs_num=2--repeat=4--temp_num=64--loop=True--test_samples=$test--merge_prompts=lstm#mlp#none --follow_method=True --scorers="rouge-bert" --data_path=${home}/mt5-comet/comet/data/atomic2020 --do_valid=False --val_samples=10 --encoder_type=lstm --cycle=100 $g1 --seed=123 --batch_size=8  
+runlite run -exp $exp -lp $home/$exp -bc base -ov $g2 -var method=unsup-wrap-nat--rel_filter=xIntent#xAttr#multi--train_samples=$train--epochs_num=2--repeat=4--temp_num=64--loop=True--test_samples=$test--merge_prompts=lstm#mlp#none --follow_method=True --scorers="rouge-bert" --data_path=${home}/mt5-comet/comet/data/atomic2020 --do_valid=False --val_samples=10 --encoder_type=lstm --cycle=100 $g1 --seed=123 --batch_size=8  
 
-
-
+tar -czvf ${home}/{$exp}.tar.gz ${home}/${exp} 
 
 
 # learning rate for supervised and unsupervised learning for t5-v1
