a6e802218ee54c1f4c8a2e53cf1621ea68ee5f0e
puraminy <pouramini@gmail.com>
2022-11-14 22:01:37 +0330

    commit

--- comet/train/data.py
+++ comet/train/data.py
@@ -29,7 +29,7 @@ class xAttrTemplate(RelTemplate):
            elif tn == 6:
                qtemp = "{event}, {emp_so_1} {emp_they_1} are {emb_seen_1} as {ph}."
            elif tn == 64:
-               qtemp = "{a_because} {a_of} {event}, {b_3} {b_they} seen as {ph}"
+               qtemp = "{c_6} {rel_4} {a_because} {a_of} {event}, {b_3} {b_they} seen as {ph}"
            elif tn == 65:
                qtemp = "{because_1} {of_1} {event}, {they_1} {seen_1} as {ph}"
            elif tn == 66:
@@ -79,7 +79,7 @@ class xIntentTemplate(RelTemplate):
            elif tn == 63:
                qtemp = "{rel_5} Because of {event}, they want {ph}"
            elif tn == 64:
-               qtemp = "{a_because} {a_of} {event}, {b_3} {b_they} want {ph}"
+               qtemp = "{c_6} {rel_4} {a_because} {a_of} {event}, {b_3} {b_they} want {ph}"
            elif tn == 652:
                qtemp = "{because_1} {of_1} {event}, {rel_4} want {ph}"
            elif tn == 653:
