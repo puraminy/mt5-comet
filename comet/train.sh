93c17f1f491afa3ab94b5dcbe921e619d6c6fcf7
puraminy <pouramini@gmail.com>
2022-11-14 22:34:57 +0330

    commit

--- comet/train.sh
+++ comet/train.sh
@@ -49,7 +49,7 @@ m=$trial
 seed=123
 log=${home}/logs/${exp}
 
-runlite run -exp $exp -lp ${log} -bc base -ov $g2 -var method=unsup-wrap-nat--rel_filter=xIntent--train_samples=$train--epochs_num=2--repeat=4--temp_num=64--loop=True--test_samples=$test--merge_prompts=none#lstm--shared_embs=False--seed=123 --follow_method=True --scorers="rouge-bert" --data_path=${home}/mt5-comet/comet/data/atomic2020 --do_valid=False --val_samples=10 --encoder_type=lstm --cycle=100 $g1 --batch_size=16 --trial=$trial 
+runlite run -exp $exp -lp ${log} -bc base -ov $g2 -var method=unsup-wrap-nat--rel_filter=xIntent#xAttr#multi--train_samples=$train--epochs_num=2--repeat=4--temp_num=64--loop=True--test_samples=$test--merge_prompts=none#lstm--shared_embs=False--seed=123 --follow_method=True --scorers="rouge-bert" --data_path=${home}/mt5-comet/comet/data/atomic2020 --do_valid=False --val_samples=10 --encoder_type=lstm --cycle=100 $g1 --batch_size=16 --trial=$trial 
 
 if [ $home = "/content/drive/MyDrive" ]; then
 	tar -czvf /content/${exp}-$m.tar.gz ${log}
