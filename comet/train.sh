b4e7432c153302c5b32f0656d547d25b23dcd155
puraminy <pouramini@gmail.com>
2022-11-15 22:59:51 +0330

    commit

--- comet/train.sh
+++ comet/train.sh
@@ -65,7 +65,7 @@ exp=xint-multi_11
 
 runlite run -exp $exp -lp ${log} -bc base -ov $g2 -var method=unsup-wrap-nat--rel_filter=$filter--train_samples=$train--epochs_num=2--repeat=4--temp_num=$tn--loop=True--test_samples=$test--merge_prompts=$merge--shared_embs=$shared--seed=123 --follow_method=True --scorers="rouge-bert" --data_path=${home}/mt5-comet/comet/data/atomic2020 --do_valid=False --val_samples=10 --encoder_type=lstm --cycle=100 $g1 --batch_size=16 --trial=$trial 
 
-cp train.sh ${log}
+#cp train.sh ${log}
 if [ $home = "/content/drive/MyDrive" ]; then
 	tar -czvf /content/${exp}-$m.tar.gz ${log}
 	cp /content/${exp}-$m.tar.gz ${home}/logs 
