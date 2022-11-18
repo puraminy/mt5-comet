2bda1fd8e8e125d2841440f69ef646ce16127f14
puraminy <pouramini@gmail.com>
2022-11-18 11:07:22 +0330

    commit

--- comet/train.sh
+++ comet/train.sh
@@ -59,11 +59,11 @@ exp=xint-multi_mergeXNat
 log=${home}/logs/${exp}
 echo "log: ${log}"
 #filter=xIntent#xAttr#xNeed#xReact#xEffect#oReact#xWant#multi
-filter=xIntent#xAttr #multi
+filter=xAttr#xIntent #multi
 merge=none #lstm
 tn=1#2
 shared=False
-trial=7
+trial=2
 epochs=3
 
 runlite run -exp $exp -lp ${log} -bc base -ov $g2 -var method=unsup-wrap-nat--rel_filter=$filter--train_samples=$train--epochs_num=$epochs--repeat=4--temp_num=$tn--loop=True--test_samples=$test--flat_prompts=$merge--shared_embs=$shared--seed=123 --follow_method=True --scorers="rouge-bert" --data_path=${home}/mt5-comet/comet/data/atomic2020 --do_valid=False --val_samples=10 --encoder_type=lstm --cycle=100 $g1 --batch_size=16 --trial=$trial 
