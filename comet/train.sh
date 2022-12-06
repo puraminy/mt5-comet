a904fb1ccf2f0b2102e1c38249e25aa38e9c6b68
Ahmad <pouramini@gmail.com>
2022-12-06 14:46:03 +0330

    commit

--- comet/train.sh
+++ comet/train.sh
@@ -66,7 +66,7 @@ elif [ "$m" -eq "1" ]; then
 fi
 seed=123
 
-exp=xint-router-merege-flat
+exp=xint-router-merge-emb
 log=${home}/logs/${exp}
 echo "log: ${log}"
 #filter=xIntent#xAttr#xNeed#xReact#xEffect#oReact#xWant#multi
@@ -76,10 +76,10 @@ tn=com-pre #mat #2#4#5
 shared=False
 trial=1
 epochs=2
-tag=temp_num@trial
+tag=temp_num@encoder_type@trial
 trunc=True
 
-runlite run -exp $exp -lp ${log} -bc base -ov $g2 -var method=unsup-wrap-nat--rel_filter=$filter--train_samples=$train--epochs_num=$epochs--repeat=4--temp_num=$tn--loop=True--test_samples=$test--flat_prompts=$merge--shared_embs=$shared--seed=123--n_prompts=1--trunc_router=$trunc--router_lr=0.001--pl_learning_rate=0.01--merge_flat=False#True --follow_method=True --scorers="rouge-bert" --data_path=${home}/mt5-comet/comet/data/atomic2020/sel --do_valid=False --val_samples=10 --encoder_type=emb --cycle=100 $g1 --batch_size=16 --trial=$trial --prompt_token_num=8 --model_id=$model --tag=$tag
+runlite run -exp $exp -lp ${log} -bc base -ov $g2 -var method=unsup-wrap-nat--rel_filter=$filter--train_samples=$train--epochs_num=$epochs--repeat=4--temp_num=$tn--loop=True--test_samples=$test--flat_prompts=$merge--shared_embs=$shared--seed=123--n_prompts=1--trunc_router=$trunc--router_lr=0.001--pl_learning_rate=0.01--encoder_type=emb#lstm --follow_method=True --scorers="rouge-bert" --data_path=${home}/mt5-comet/comet/data/atomic2020/sel --do_valid=False --val_samples=10  --cycle=100 $g1 --batch_size=16 --trial=$trial --prompt_token_num=8 --model_id=$model --tag=$tag
 
 cp train.sh ${log}
 #if [ $home = "/content" ]; then
